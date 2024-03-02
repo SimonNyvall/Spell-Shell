00a44e799efd4fce83068d919f5afdef55c2743e
SimonNyvall <simon.k.nyvall@proton.me>
2024-03-02 15:44:37 +0100

    add: main program

--- src/spell-shell/Program.cs
+++ src/spell-shell/Program.cs
@@ -1,13 +1,38 @@
-﻿using SpellShell.SpellDistance;
+﻿using System.Text.Json;
+using SpellShell.SpellDistance;
 
-var table = SpellDistance.GetDistance(" boats", " float");
-
-for (int i = 0; i < 6; i++)
+if (args.Length != 1)
 {
-    for (int j = 0; j < 6; j++)
-    {
-        Console.Write(table[i, j] + " ");
-    }
-    Console.WriteLine();
+    Console.WriteLine("Please provide a word to spell check");
+    return;
 }
 
+var word = args[0];
+
+var json = await File.ReadAllTextAsync("programming-terms.json");
+var programmingTerms = JsonSerializer.Deserialize<ProgrammingTerms>(json);
+
+var scoredWords = new List<(string, int)>();
+
+foreach (var term in programmingTerms!.Terms!)
+{
+    var distance = SpellDistance.GetDistance(word, term);
+    scoredWords.Add((term, distance));
+}
+
+scoredWords.Sort((a, b) => a.Item2.CompareTo(b.Item2));
+
+var results = scoredWords
+    .Take(5)
+    .Select(x => x.Item1);
+
+foreach (var result in results)
+{
+    Console.WriteLine(result);
+}
+
+
+class ProgrammingTerms
+{
+    public List<string>? Terms { get; set; }
+}
